f0148e94e0d755da3b87b79a3061625cfb63c6fc
Alina Strashevichutse <astrashevichute@gmail.com>
2022-03-27 14:30:12 +0200

    add test to check login and nav-item color

--- specs/login-layout.e2e.js
+++ specs/login-layout.e2e.js
@@ -3,13 +3,28 @@ describe('Check app', function () {
     it('should login', async function () {
         await browser.url('https://viktor-silakov.github.io/course-sut');
         await $('#login').setValue('walker@jw.com');
+        const login = await $('#login').getValue();
         await $('#password').setValue('password');
+        await browser.pause(1000);
         await $('button').click();
-        await browser.pause(15000);
-        const title = await browser.getTitle();
-        if (title !== 'Report portal') {
-            throw new Error('You don`t login into system!!!')
+        await $('#spinner').waitForDisplayed({ reverse: true, timeout: 10500 });
+        const user = await $('#user-label').getAttribute('title');
+        if(login !== user){
+            throw new Error('You don`t login into system with right user!!!');
         }
+
+        await browser.pause(3000);
+
+        const navItems = await $$('//*[@id="first-nav-block"]//*[contains(@class, "nav-item")]');        
+        for (const item of navItems){
+            await item.moveTo();
+            await browser.pause(1500);
+            let color = await item.getCSSProperty("background-color");
+            if(color.value === 'rgba(255,0,0,1)'){
+                const text = await item.getText();
+                throw new Error(`Item ${text} has wrong color`);
+            }
+        }         
     });
 });
 
