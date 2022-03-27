60c49858b147e4c97d9753ca16cf16f0222901a3
Alina Strashevichutse <astrashevichute@gmail.com>
2022-03-27 14:38:30 +0200

    add test to click the hide button

--- specs/click-hide-button.e2e.js
+++ specs/click-hide-button.e2e.js
@@ -4,10 +4,16 @@ describe('Check app', function () {
         await browser.url('https://viktor-silakov.github.io/course-sut');
         await $('#login').setValue('walker@jw.com');
         await $('#password').setValue('password');
+        await browser.pause(1000);
         await $('button').click();
+        const header = await $('header');
+        await browser.pause(10000);
+        await browser.execute((elem) => elem.remove(), header);
+        await browser.pause(5000);
+        await $('//*[@id="dashboard"]/div[1]/div/div/button').click();
         await browser.pause(3000);
-        await browser.execute('alert(arguments[0] + arguments[1])', 'Hello', ' World!');
-        await browser.pause(3000);
+        await browser.acceptAlert();
+        await browser.pause(1000);
     });
 });
 
